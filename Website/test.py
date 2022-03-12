1de893e29b11b8f093ba4f9c7f4a6c6a1a11d388
Isaac Rattey <irattey@umd.edu>
2022-03-12 14:25:23 -0500

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', debug=True, port=5000)
+
 #background process happening without any refreshing
 @app.route('/button_test')
 def button_test():
