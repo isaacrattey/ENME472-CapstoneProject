6fb0084f4a794ec3a55176587816af2e11eac689
Isaac Rattey <irattey@umd.edu>
2022-04-16 13:20:57 -0400

    Update main.py

--- Sensing/main.py
+++ Sensing/main.py
@@ -2,10 +2,14 @@ from Sensor import Sensor
 import time
 
 phChn = 0
+tempChn = 1
 address = 0x48
 
 phSensor = Sensor(address, phChn)
+tempSensor = Sensor(address, tempChn)
 
 while True:
-    print("%4f" % phSensor.getReading())
+    ph = phSensor.getReading()
+    temp = tempSensor.getReading()
+    print(f"pH: {ph}\tTemp: {temp}")
     time.sleep(0.1)
