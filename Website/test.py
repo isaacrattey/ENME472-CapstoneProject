6eebc888cca7adcbc559e82e247d86e95bf96c73
Jizh10 <jzheng31415@gmail.com>
2022-04-24 17:08:33 -0400

    button test

--- Website/test.py
+++ Website/test.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+from datetime import datetime
 
 #init flask application
 app = Flask(__name__)
@@ -6,10 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    time1 = str(datetime.datetime.now())
+    time2 = str(datetime.datetime.now())
+    pH1 = -1
+    pH2 = -2
+    moisture1 = 0
+    moisture2 = 0
+    return render_template('index.html', time1, time2, pH1, pH2, moisture1, moisture2)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', debug=True, port=5000)
+
 
 #background process happening without any refreshing
 @app.route('/button_test')
