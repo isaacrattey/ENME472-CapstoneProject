641d51417826394a1bec3f362f7ac266dd25102f
Isaac Rattey <ratteyisaac@gmail.com>
2022-04-24 22:30:04 +0100

    changes

--- Website/test.py
+++ Website/test.py
@@ -18,7 +18,7 @@ def index():
 if __name__ == '__main__':
       app.run(host='0.0.0.0', debug=True, port=5000)
 
-@app.route('collectDataButton/<trayNum>')
+@app.route('/collectDataButton/<trayNum>')
 def collectDataButton(trayNum):
     if trayNum == 1:
         #move to tray1
