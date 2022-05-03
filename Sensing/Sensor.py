698766ba2d9f0e80ba2e2460aac714b3cdaf4e3b
Isaac Rattey <irattey@umd.edu>
2022-05-03 17:16:36 -0400

    move things around

---
+++ Sensing/Sensor.py
@@ -0,0 +1,10 @@
+from PCF8591 import PCF8591
+
+class Sensor:
+
+    def __init__(self, address, chn):
+        self.adc = PCF8591(address)
+        self.senseChn = chn
+
+    def getReading(self):
+        return self.adc.read(self.senseChn)
