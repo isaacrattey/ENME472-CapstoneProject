f51d53c81159652fb782badeeba42d1113500bc8
Isaac Rattey <irattey@umd.edu>
2022-05-05 11:39:15 -0400

    Update moveall.py

--- Website/moving/moveall.py
+++ Website/moving/moveall.py
@@ -5,31 +5,32 @@ import time
 z = moving.zaxis.zaxis(step = 19, dir = 13)
 xy = moving.xyaxis.xyaxis()
 
-xy.move(0,0)
-z.move(150)
-xy.move(0,70)
-time.sleep(3)
+while True:
+    xy.move(0,0)
+    z.move(150)
+    xy.move(0,70)
+    time.sleep(3)
 
 
-xy.move(0,0)
-z.move(0)
-time.sleep(3)
+    xy.move(0,0)
+    z.move(0)
+    time.sleep(3)
 
-z.move(0)
-xy.move(300,380)
-z.move(150)
-time.sleep(3)
-
-
-
-xy.move(300,440)
-time.sleep(3)
+    z.move(0)
+    xy.move(300,380)
+    z.move(150)
+    time.sleep(3)
 
 
 
-xy.move(300,380)
-z.move(0)
-xy.move(0,0)
-time.sleep(3)
+    xy.move(300,440)
+    time.sleep(3)
+
+
+
+    xy.move(300,380)
+    z.move(0)
+    xy.move(0,0)
+    time.sleep(3)
 
 
