24560a4d0d8492b1d0cc9d0375819766c654a281
Isaac Rattey <irattey@umd.edu>
2022-05-05 00:56:51 -0400

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -49,3 +49,101 @@ def index():
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', debug=True, port=5000)
+
+      @app.route('/collectDataButton/<trayNum>')
+def collectDataButton(trayNum):
+    df = pd.read_pickle("./plantData.pkl")
+    # print(trayNum)
+    if str(trayNum) == "1":
+        
+        #move to tray1
+        # move to tray position
+        # already done
+        # move z into position
+        z.move(150)
+        # move y down into tray
+        xy.move(0, 130)
+        #collect data for tray1
+        pH1, temp, moisture1 = PlantTester.measure()
+        #update data for tray1
+        data={
+            'Time':datetime.now(),
+            'Tray':1,
+            'Moisture':moisture1,
+            'pH':pH1,
+            'Temperature':temp
+        }
+        df = df.append(data, ignore_index=True)
+        print(data)
+
+        #output data to thingspeak
+        api = "MAM1MDSXCO7T18KT"
+        # params = {1:pH1, 2:moisture1, "api_key":api}
+        # params = urlencode(params)
+        # url = "https://api.thingspeak.com/update?" + params
+        url = "https://api.thingspeak.com/update?api_key=" + api + "&field1=" + str(pH1) + "&field2=" + str(moisture1)
+        # url = "https://api.thingspeak.com/update?api_key=" + api + "&field1=" + str(pH1)
+        # response = urlopen(url)
+        # url = "https://api.thingspeak.com/update?api_key=" + api + "&field2=" + str(moisture1)
+        response = urlopen(url)
+        # print(response.status, response.reason)
+
+        # Return to neutral position
+        xy.move(0,0)
+        z.move(0)
+
+    else:
+        #move to tray2
+        xy.move(300, 380)
+        z.move(150)
+        xy.move(300, 440)
+        #collect data for tray2
+        pH2, temp, moisture2 = PlantTester.measure()
+        #update data for tray2
+        data={
+            'Time':datetime.now(),
+            'Tray':2,
+            'Moisture':moisture2,
+            'pH':pH2,
+            'Temperature':temp
+        }
+        df = df.append(data, ignore_index=True)
+        
+
+        #output data to thingspeak
+        api = "MAM1MDSXCO7T18KT"
+        # params = {3:pH2, 4:moisture2, "api_key":api}
+        # params = urlencode(params)
+        # url = "https://api.thingspeak.com/update?" + params
+        url = "https://api.thingspeak.com/update?api_key=" + api + "&field3=" + str(pH2) + "&field4=" + str(moisture2)
+        # url = "https://api.thingspeak.com/update?api_key=" + api + "&field3=" + str(pH2)
+        # response = urlopen(url)
+        # url = "https://api.thingspeak.com/update?api_key=" + api + "&field4=" + str(moisture2)
+        response = urlopen(url)
+        # print(response.status, response.reason)
+
+        # Return to neutral position
+        xy.move(300, 380)
+        z.move(0)
+        xy.move(0,0)
+
+
+    
+	# Read data from pickle
+	#get last data for tray1
+    # pH1 = df.loc[df.Tray == 1].iloc[-1].pH
+    # moisture1 = df.loc[df.Tray == 1].iloc[-1].Moisture
+    # time1 = df.loc[df.Tray == 1].iloc[-1].Time
+
+	# #get last data for tray2
+    # pH2 = df.loc[df.Tray == 2].iloc[-1].pH
+    # moisture2 = df.loc[df.Tray == 2].iloc[-1].Moisture
+    # time2 = df.loc[df.Tray == 2].iloc[-1].Time
+	
+    # Save Data
+    df.to_pickle("./plantData.pkl")
+
+	# Display data
+    # render_template('index.html', time1=time1, time2=time2, pH1=pH1, pH2=pH2, moisture1=moisture1, moisture2=moisture2)
+
+    return redirect(request.referrer)
