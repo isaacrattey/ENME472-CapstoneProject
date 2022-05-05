9ba7eaab66cbec338d126cf7f881fde15bdd8035
Isaac Rattey <ratteyisaac@gmail.com>
2022-05-05 15:45:55 +0100

    changes

--- Website/moving/moveall.py
+++ Website/moving/moveall.py
@@ -1,9 +1,9 @@
-import xyaxis
-import zaxis
+import moving.xyaxis
+import moving.zaxis
 import time
 
-z = zaxis.zaxis(step = 19, dir = 13)
-xy = xyaxis.xyaxis()
+z = moving.zaxis.zaxis(step = 19, dir = 13)
+xy = moving.xyaxis.xyaxis()
 
 xy.move(0,0)
 z.move(150)
